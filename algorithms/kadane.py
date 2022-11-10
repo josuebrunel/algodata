88d4cd64411f89787cbc66c97d83b6054c15af98
Josue Kouka <josuebrunel@gmail.com>
2022-11-09 16:12:03 -0800

    algo: improve kadane's algo

--- algorithms/kadane.py
+++ algorithms/kadane.py
@@ -1,22 +1,49 @@
-def kadane(array):
-    msum = float("-inf")
-    csum = float("-inf")
-    for i in array:
-        csum = max(i, csum + i)
-        msum = max(msum, csum)
-        print(csum, msum)
-    return msum
+import unittest
+
+
+def kadaneMaxSubarray(array):
+    maxSum = float("-inf")
+    curSum = 0
+
+    for n in array:
+        curSum = max(n, n + curSum)
+        maxSum = max(maxSum, curSum)
+
+    return maxSum
+
+
+def kadaneMaxSubarrayIJ(array):
+    L = 0
+    maxL, maxR = 0, 0
+    curSum, maxSum = 0, float("-inf")
+
+    for R in range(len(array)):
+        if array[R] > curSum + array[R]:
+            L = R
+            curSum = array[R]
+        else:
+            curSum += array[R]
+
+        if curSum > maxSum:
+            maxSum = curSum
+            maxL, maxR = L, R
+
+    return array[maxL], array[maxR]
+
+
+class KadaneTest(unittest.TestCase):
+
+    def test_maximum_subarray(self):
+        self.assertEqual(kadaneMaxSubarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6)
+        self.assertEqual(kadaneMaxSubarray([1]), 1)
+        self.assertEqual(kadaneMaxSubarray([5, 4, -1, 7, 8]), 23)
+
+    def test_maximum_subarray_ij(self):
+        self.assertEqual(kadaneMaxSubarrayIJ([-2, 1, -3, 4, -1, 2, 1, -5, 4]),
+                         (4, 1))
+        self.assertEqual(kadaneMaxSubarrayIJ([1]), (1, 1))
+        self.assertEqual(kadaneMaxSubarrayIJ([5, 4, -1, 7, 8]), (5, 8))
+
 
 if __name__ == "__main__":
-    array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-    assert kadane(array) == 6 # with [4, -1, 2, 1]
-
-    # max(-2, 0-2) = -2, -2 
-    # max(1, 1-2) = 1, 1
-    # max(-3, -3+1 ) = -2, 1
-    # max(4, 4-2) = 4, 4 
-    # max(-1, -1+4) = 3, 4
-    # max(2, 2+3 ) = 5, 5
-    # max(1, 1+5) = 6, 6
-    # max(-5, -5+6) = 1, 6
-    # max(4, 4+1) = 5, 6
+    unittest.main()
