95d19147e821e8ed43b2b00b7fe7f0aaf32d37b5
Josue Kouka <josuebrunel@gmail.com>
2022-11-26 15:51:36 -0800

    leetcode: fix 211 Design Add ans Search Word solution

--- sandbox/leetcode/211.Design.Add.and.Search.Word.DS.py
+++ sandbox/leetcode/211.Design.Add.and.Search.Word.DS.py
@@ -31,7 +31,7 @@ class WordDictionary:
                     for child in cur.children.values():
                         if dfs(i + 1, child):
                             return True
-                        return False
+                    return False
                 else:
                     if c not in cur.children:
                         return False
@@ -52,6 +52,15 @@ class WordDictionaryTest(unittest.TestCase):
         self.assertEqual(wd.search("bad"), True)
         self.assertEqual(wd.search(".ad"), True)
         self.assertEqual(wd.search("b.."), True)
+        wd = WordDictionary()
+        wd.addWord("a")
+        wd.addWord("a")
+        self.assertEqual(wd.search("."), True)
+        self.assertEqual(wd.search("a"), True)
+        self.assertEqual(wd.search("aa"), False)
+        self.assertEqual(wd.search("a"), True)
+        self.assertEqual(wd.search(".a"), False)
+        self.assertEqual(wd.search("a."), False)
 
 
 if __name__ == "__main__":
